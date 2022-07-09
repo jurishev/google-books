c0424b41e7647eac2e3d5c855565849355672b2a
Yuri Shevchenko <jurishev@ya.ru>
2022-07-09 10:53:39 +0300

    Refactor UI

--- GoogleBooks/ClientApp/src/app/spinner/spinner.component.ts
+++ GoogleBooks/ClientApp/src/app/spinner/spinner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
     selector: 'app-spinner',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
     styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent {
+
+    color: ThemePalette = "accent";
 }
