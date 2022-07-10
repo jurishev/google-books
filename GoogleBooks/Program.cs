97db7e57c827f09a76f047a03a3463657694fc2b
Yuri Shevchenko <jurishev@ya.ru>
2022-07-10 22:30:33 +0300

    Clean up

--- GoogleBooks/Program.cs
+++ GoogleBooks/Program.cs
@@ -2,44 +2,32 @@ using Microsoft.EntityFrameworkCore;
 using GoogleBooks.Services;
 using GoogleBooks.Storage;
 
-namespace GoogleBooks
+namespace GoogleBooks;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+        builder.Services.AddControllersWithViews();
 
-            builder.Services.AddControllersWithViews();
+        builder.Services
+            .AddScoped<IGoogleBooksStorage, EntityFrameworkGoogleBooksStorage>()
+            .AddScoped<IGoogleBooksService, StoringGoogleBooksService>()
+            .AddScoped<HttpClient>();
 
-            builder.Services
-                .AddScoped<IGoogleBooksStorage, EntityFrameworkGoogleBooksStorage>()
-                .AddScoped<IGoogleBooksService, StoringGoogleBooksService>()
-                .AddScoped<HttpClient>();
+        builder.Services.AddDbContext<GoogleBooksContext>(
+            options => options.UseInMemoryDatabase("GoogleBooks"));
 
-            builder.Services.AddDbContext<GoogleBooksContext>(
-                options => options.UseInMemoryDatabase("GoogleBooks"));
+        var app = builder.Build();
 
-            var app = builder.Build();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.MapControllers();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-            }
+        app.MapFallbackToFile("index.html");
 
-            app.UseStaticFiles();
-            app.UseRouting();
-
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller}/{action=Index}/{id?}");
-
-            app.MapFallbackToFile("index.html");
-
-            app.Run();
-        }
+        app.Run();
     }
 }
--- GoogleBooks/Services/StoringGoogleBooksService.cs
+++ GoogleBooks/Services/StoringGoogleBooksService.cs
@@ -1,112 +1,111 @@
 ﻿using GoogleBooks.Models;
 using GoogleBooks.Storage;
 
-namespace GoogleBooks.Services
+namespace GoogleBooks.Services;
+
+public class StoringGoogleBooksService : IGoogleBooksService
 {
-    public class StoringGoogleBooksService : IGoogleBooksService
+    private readonly IGoogleBooksStorage storage;
+    private readonly HttpClient httpClient;
+
+    public StoringGoogleBooksService(IGoogleBooksStorage storage, HttpClient httpClient)
     {
-        private readonly IGoogleBooksStorage storage;
-        private readonly HttpClient httpClient;
+        this.storage = storage;
+        this.httpClient = httpClient;
+    }
 
-        public StoringGoogleBooksService(IGoogleBooksStorage storage, HttpClient httpClient)
+    public async Task<Bookshelf> GetBookshelf(int shelf, string userId)
+    {
+        var bookshelf = this.storage.GetBookshelf(shelf, userId);
+        if (bookshelf is null)
         {
-            this.storage = storage;
-            this.httpClient = httpClient;
-        }
-
-        public async Task<Bookshelf> GetBookshelf(int shelf, string userId)
-        {
-            var bookshelf = this.storage.GetBookshelf(shelf, userId);
-            if (bookshelf is null)
-            {
-                bookshelf = await this.CallGoogleApi<Bookshelf>($"https://www.googleapis.com/books/v1/users/{userId}/bookshelves/{shelf}");
-                if (bookshelf != null)
-                {
-                    this.storage.AddBookshelf(bookshelf, userId);
-                }
-            }
-
+            bookshelf = await this.CallGoogleApi<Bookshelf>($"https://www.googleapis.com/books/v1/users/{userId}/bookshelves/{shelf}");
             if (bookshelf != null)
             {
-                bookshelf.Volumes = (await this.CallGoogleApi<VolumeList>($"https://www.googleapis.com/books/v1/users/{userId}/bookshelves/{shelf}/volumes"))?.Items;
+                this.storage.AddBookshelf(bookshelf, userId);
             }
-
-            return bookshelf;
         }
 
-        public async Task<IEnumerable<Bookshelf>> GetBookshefList(string userId)
+        if (bookshelf != null)
         {
-            var bookshelves = (await this.CallGoogleApi<BookshelfList>($"https://www.googleapis.com/books/v1/users/{userId}/bookshelves"))?.Items;
-            if (bookshelves != null)
-            {
-                foreach (var bookshelf in bookshelves)
-                {
-                    this.storage.AddBookshelf(bookshelf, userId);
-                }
-            }
-
-            return bookshelves;
+            bookshelf.Volumes = (await this.CallGoogleApi<VolumeList>($"https://www.googleapis.com/books/v1/users/{userId}/bookshelves/{shelf}/volumes"))?.Items;
         }
 
-        public async Task<Volume> GetVolume(string volumeId)
+        return bookshelf;
+    }
+
+    public async Task<IEnumerable<Bookshelf>> GetBookshefList(string userId)
+    {
+        var bookshelves = (await this.CallGoogleApi<BookshelfList>($"https://www.googleapis.com/books/v1/users/{userId}/bookshelves"))?.Items;
+        if (bookshelves != null)
         {
-            var volume = this.storage.GetVolume(volumeId);
-            if (volume is null)
+            foreach (var bookshelf in bookshelves)
             {
-                volume = await this.CallGoogleApi<Volume>($"https://www.googleapis.com/books/v1/volumes/{volumeId}");
-                if (volume != null)
+                this.storage.AddBookshelf(bookshelf, userId);
+            }
+        }
+
+        return bookshelves;
+    }
+
+    public async Task<Volume> GetVolume(string volumeId)
+    {
+        var volume = this.storage.GetVolume(volumeId);
+        if (volume is null)
+        {
+            volume = await this.CallGoogleApi<Volume>($"https://www.googleapis.com/books/v1/volumes/{volumeId}");
+            if (volume != null)
+            {
+                this.storage.AddVolume(volume);
+            }
+        }
+
+        return volume;
+    }
+
+    public async Task<IEnumerable<Volume>> GetVolumeList(VolumeQuery query)
+    {
+        var volumes = this.storage.GetVolumeList(query);
+        if (volumes is null)
+        {
+            var terms = GetTerms(query);
+            volumes = (await this.CallGoogleApi<VolumeList>($"https://www.googleapis.com/books/v1/volumes?q={terms}"))?.Items;
+            if (volumes != null)
+            {
+                foreach (var volume in volumes)
                 {
                     this.storage.AddVolume(volume);
                 }
             }
-
-            return volume;
         }
 
-        public async Task<IEnumerable<Volume>> GetVolumeList(VolumeQuery query)
+        return volumes;
+    }
+
+    private async Task<T> CallGoogleApi<T>(string uri)
+    {
+        var result = await this.httpClient.GetAsync(uri);
+        return result.IsSuccessStatusCode ? await result.Content.ReadFromJsonAsync<T>() : default;
+    }
+
+    private static string GetTerms(VolumeQuery query)
+    {
+        var terms = new List<string>();
+
+        AddTerm("intitle", query.Title, terms);
+        AddTerm("inauthor", query.Author, terms);
+        AddTerm("inpublisher", query.Publisher, terms);
+        AddTerm("subject", query.Subject, terms);
+        AddTerm("isbn", query.Isbn, terms);
+
+        return string.Join('+', terms);
+    }
+
+    private static void AddTerm(string key, string value, List<string> terms)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
         {
-            var volumes = this.storage.GetVolumeList(query);
-            if (volumes is null)
-            {
-                var terms = GetTerms(query);
-                volumes = (await this.CallGoogleApi<VolumeList>($"https://www.googleapis.com/books/v1/volumes?q={terms}"))?.Items;
-                if (volumes != null)
-                {
-                    foreach (var volume in volumes)
-                    {
-                        this.storage.AddVolume(volume);
-                    }
-                }
-            }
-
-            return volumes;
-        }
-
-        private async Task<T> CallGoogleApi<T>(string uri)
-        {
-            var result = await this.httpClient.GetAsync(uri);
-            return result.IsSuccessStatusCode ? await result.Content.ReadFromJsonAsync<T>() : default;
-        }
-
-        private static string GetTerms(VolumeQuery query)
-        {
-            var terms = new List<string>();
-
-            AddTerm("intitle", query.Title, terms);
-            AddTerm("inauthor", query.Author, terms);
-            AddTerm("inpublisher", query.Publisher, terms);
-            AddTerm("subject", query.Subject, terms);
-            AddTerm("isbn", query.Isbn, terms);
-
-            return string.Join('+', terms);
-        }
-
-        private static void AddTerm(string key, string value, List<string> terms)
-        {
-            if (!string.IsNullOrWhiteSpace(value))
-            {
-                terms.Add($"{key}:{value}");
-            }
+            terms.Add($"{key}:{value}");
         }
     }
 }
